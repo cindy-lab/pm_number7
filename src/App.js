596f211be131b0e4ce68c646c719e8e0c2bc2d68
cindy-lab <cindy.binondo@student.passerellesnumeriques.org>
2019-09-28 23:28:48 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      array : [
+        {firstname : "Juan",
+         lastname: "De la Cruz",
+         age: 21
+        },
+
+        { firstname: "Mark",
+        lastname: "Paul",
+        age: 22
+        },
+
+        {firstname: "Paul",
+        lastname: "Mark",
+        age: 23
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.array.map((item, index) => (
+            <h3 key={index}>
+              {item.firstname + "  "+ item.lastname + " , " + item.age}
+            </h3>
+          ))}
+        </h1>
+      </div>
+    );
+  }
+}
+    
+export default App;
+
